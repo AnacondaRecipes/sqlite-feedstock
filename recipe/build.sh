30cae11eafe278e75f997dd1566170c392bc992b
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-07-16 15:00:07 +0200

    remove autoconf/libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,6 @@ if [[ "${BUILD}" != "${HOST}" ]]; then
   export PATH=${PWD}:$PATH
 fi
 
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
-
 export CPPFLAGS="${CPPFLAGS} -DSQLITE_ENABLE_COLUMN_METADATA=1 \
                              -DSQLITE_ENABLE_UNLOCK_NOTIFY \
                              -DSQLITE_ENABLE_DBSTAT_VTAB=1 \
